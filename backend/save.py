90b0386c5e6a2723ce06016f5bc7c753b1aba271
andndre <agungandre687@gmail.com>
2024-12-17 19:09:02 +0800

    remove kemnaker

--- backend/save.py
+++ backend/save.py
@@ -44,47 +44,49 @@ class Job:
         rdf_graph.add((job_uri, EX.source, Literal(self.source)))
         return job_uri
 
-def scrape_kemnaker(keyword=None):
-    base_url = "https://karirhub.kemnaker.go.id/lowongan-dalam-negeri/lowongan"
-    url = f"{base_url}?filters=keyword:%23{quote(keyword)}" if keyword else base_url
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, "html.parser")
+# def scrape_kemnaker(keyword=None):
+#     base_url = "https://karirhub.kemnaker.go.id/lowongan-dalam-negeri/lowongan"
+#     url = f"{base_url}?filters=keyword:{quote(keyword)}%23{quote(keyword)}" if keyword else base_url
+#     headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+#     }
+#     response = requests.get(url, headers=headers)
+#     response.raise_for_status()
+#     soup = BeautifulSoup(response.text, "html.parser")
 
-    job_elements = soup.find_all("sisnaker-element-karirhub-domestic-vacancy-card-web")
+#     job_elements = soup.find_all("sisnaker-element-karirhub-domestic-vacancy-card-web")
+#     print(url)
+#     jobs = []
+#     for job_element in job_elements:
+#         try:
+#             job_title_element = job_element.find("div", class_="text-sm font-bold text-grey-700")
+#             job_title = extract_text(job_title_element)
+#             company_element = job_element.find("div", class_="text-sm text-grey-700")
+#             location_element = job_element.find("div", class_="text-xs text-grey-500")
+#             salary_element = job_element.find("div", class_="font-medium text-grey-700")
 
-    jobs = []
-    for job_element in job_elements:
-        job_title_element = job_element.find("div", class_="text-sm font-bold text-grey-700")
-        job_title = extract_text(job_title_element)
-        
-        company_element = job_element.find("div", class_="text-sm text-grey-700")
-        location_element = job_element.find("div", class_="text-xs text-grey-500")
-        salary_element = job_element.find("div", class_="font-medium text-grey-700")
+#             company_name = extract_text(company_element, "No company")
+#             location = extract_text(location_element, "No location")
+#             salary = extract_text(salary_element, "No salary")
+#             if "IDR" not in salary:
+#                 salary = "No salary"
 
-        company_name = extract_text(company_element, "No company")
-        location = extract_text(location_element, "No location")
-        salary = extract_text(salary_element, "No salary")
-        if "IDR" not in salary:
-            salary = "No salary"
+#             company = Company(company_name, location)
+#             job = Job(job_title, company, url, "Kemnaker", salary)
+#             job.to_rdf()
 
-        company = Company(company_name, location)
-        job = Job(job_title, company, url, "Kemnaker", salary)
-        job.to_rdf()
+#             jobs.append({
+#                 "title": job_title,
+#                 "salary": salary,
+#                 "company": company_name,
+#                 "location": location,
+#                 "job_url": url,
+#                 "source": "Kemnaker"
+#             })
+#         except Exception:
+#             continue
 
-        jobs.append({
-            "title": job_title,
-            "salary": salary,
-            "company": company_name,
-            "location": location,
-            "job_url": url,
-            "source": "Kemnaker"
-        })
-
-    return jobs
+#     return jobs
 
 def scrape_kitalulus(keyword=None):
     base_url = "https://www.kitalulus.com/lowongan"
@@ -101,37 +103,40 @@ def scrape_kitalulus(keyword=None):
     
     jobs = []
     for job_element in job_elements:
-        job_title_element = job_element.find("h3", class_="TextStyled__H3-sc-18vo2dc-3 eHZQKp")
-        job_title = extract_text(job_title_element)
-        
-        a_tag = job_element.find("a", href=True)
-        job_url = "https://www.kitalulus.com" + a_tag["href"] if a_tag else "No URL"
+        try:
+            job_title_element = job_element.find("h3", class_="TextStyled__H3-sc-18vo2dc-3 eHZQKp")
+            job_title = extract_text(job_title_element)
+            
+            a_tag = job_element.find("a", href=True)
+            job_url = "https://www.kitalulus.com" + a_tag["href"] if a_tag else "No URL"
 
-        company_element = job_element.find("p", class_="TextStyled__Text-sc-18vo2dc-0 kaIrsv")
-        location_element = job_element.find("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb")
-        qualification_element = location_element.find_next("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb") if location_element else None
-        salary_element = qualification_element.find_next("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb") if qualification_element else None
+            company_element = job_element.find("p", class_="TextStyled__Text-sc-18vo2dc-0 kaIrsv")
+            location_element = job_element.find("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb")
+            qualification_element = location_element.find_next("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb") if location_element else None
+            salary_element = qualification_element.find_next("p", class_="CardRectangleStyled__Text-sc-1lom4v1-8 drzZmb") if qualification_element else None
 
-        company_name = extract_text(company_element, "No company")
-        location = extract_text(location_element, "No location")
-        salary = extract_text(salary_element, "No salary")
-        if "Rp" not in salary:
-            salary = "No salary"
-        
-        print(job_title, company_name, location, salary, job_url)
+            company_name = extract_text(company_element, "No company")
+            location = extract_text(location_element, "No location")
+            salary = extract_text(salary_element, "No salary")
+            if "Rp" not in salary:
+                salary = "No salary"
+            
+            print(job_title, company_name, location, salary, job_url)
 
-        company = Company(company_name, location)
-        job = Job(job_title, company, job_url, "KitaLulus", salary)
-        job.to_rdf()
+            company = Company(company_name, location)
+            job = Job(job_title, company, job_url, "KitaLulus", salary)
+            job.to_rdf()
 
-        jobs.append({
-            "title": job_title,
-            "salary": salary,
-            "company": company_name,
-            "location": location,
-            "job_url": job_url,
-            "source": "KitaLulus"
-        })
+            jobs.append({
+                "title": job_title,
+                "salary": salary,
+                "company": company_name,
+                "location": location,
+                "job_url": job_url,
+                "source": "KitaLulus"
+            })
+        except Exception:
+            continue
 
     return jobs
 
@@ -148,30 +153,33 @@ def scrape_jobstreet(keyword=None):
 
     jobs = []
     for item in soup.find_all('article', class_='_1unphw40 _1unphw41 tcmsgw8n tcmsgw8o tcmsgw7j tcmsgw7k tcmsgwav tcmsgwaw tcmsgw9r tcmsgw9s tcmsgwh tcmsgw67 tcmsgw5f s57onkb s57onk9 s57onka _1siu89c18 _1siu89c1b tcmsgw33 tcmsgw36'):
-        job_url = 'https://id.jobstreet.com' + item.find('a')['href']
-        title = item.find('a', {'data-automation': 'jobTitle'}).text.strip()
-        company_name = item.find('a', {'data-automation': 'jobCompany'}).text.strip()
-        location = item.find('span', {'data-automation': 'jobCardLocation'}).text.strip()
-        salary = extract_text(item.find('span', {'data-automation': 'jobSalary'}), "No salary")
+        try:
+            job_url = 'https://id.jobstreet.com' + item.find('a')['href']
+            title = extract_text(item.find('a', {'data-automation': 'jobTitle'}), "Invalid")
+            company_name = extract_text(item.find('a', {'data-automation': 'jobCompany'}), "Invalid")
+            location = extract_text(item.find('span', {'data-automation': 'jobCardLocation'}), "Invalid")
+            salary = extract_text(item.find('span', {'data-automation': 'jobSalary'}), "No salary")
 
-        company = Company(name=company_name, location=location)
-        job = Job(title=title, company=company, job_url=job_url, source="Jobstreet", salary=salary)
-        job.to_rdf()
+            company = Company(name=company_name, location=location)
+            job = Job(title=title, company=company, job_url=job_url, source="Jobstreet", salary=salary)
+            job.to_rdf()
 
-        jobs.append({
-            "title": title,
-            "company": company_name,
-            "location": location,
-            "salary": salary,
-            "job_url": job_url,
-            "source": "Jobstreet"
-        })
+            jobs.append({
+                "title": title,
+                "company": company_name,
+                "location": location,
+                "salary": salary,
+                "job_url": job_url,
+                "source": "Jobstreet"
+            })
+        except Exception:
+            continue
 
     return jobs
 
 def search_and_store_jobs(keyword=None):
     jobs = []
-    jobs.extend(scrape_kemnaker(keyword))
+    # jobs.extend(scrape_kemnaker(keyword))
     jobs.extend(scrape_kitalulus(keyword))
     jobs.extend(scrape_jobstreet(keyword))
     
@@ -234,17 +242,12 @@ def upload_to_graphdb_via_sparql(graph, sparql_endpoint_url):
     else:
         print(f"Gagal mengunggah data RDF melalui SPARQL: {response.status_code} {response.text}")
 
-if __name__ == "__main__":
-    graphdb_url = os.getenv("GRAPHDB_ENDPOINT")  + '/repositories/lokerku' + "/statements"
-    # Menjalankan fungsi scraping dan menyimpan data
-    jobs = search_and_store_jobs()
+# if __name__ == "__main__":
+#     graphdb_url = os.getenv("GRAPHDB_ENDPOINT")  + '/repositories/lokerku' + "/statements"
 
-    # URL repository GraphDB
-    # Menghapus semua instance Job dan Company dari GraphDB
-    delete_all_jobs_and_companies(graphdb_url)
+#     # URL repository GraphDB
+#     # Menghapus semua instance Job dan Company dari GraphDB
+#     delete_all_jobs_and_companies(graphdb_url)
 
-    # Mengunggah data RDF ke GraphDB
-    upload_to_graphdb_via_sparql(rdf_graph, graphdb_url)
-
-    # Print jobs for verification
-    print("All Jobs:", jobs)
+#     # Mengunggah data RDF ke GraphDB
+#     upload_to_graphdb_via_sparql(rdf_graph, graphdb_url)
